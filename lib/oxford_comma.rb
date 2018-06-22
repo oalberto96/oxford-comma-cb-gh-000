b3dc1611045f0661ffdcee25180338898545da9e
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-22 03:25:31 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,10 @@
 def oxford_comma(array)
   result = ""
   array.each_with_index do |item, index|
+    if array.size -1 == index
+      result << item
+      break
+    end
     if array.size > 1
       result << "#{item} #{index < array.size - 2 ? ", " : " and " }"
     end
