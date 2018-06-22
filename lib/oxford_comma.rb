0632bdcfbf8b99e8dd6d8abde4c6fa930deed4d8
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-22 02:57:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   result = ""
   array.each_with_index do |item, index|
-    puts result << item + (index + 1 < array.size - 1 ? ", " : " and ")
+    puts result << item + (index  < array.size - 1 ? ", " : " and ")
   end
 end
