781ff9a739b29bfa030c05e7f804382f55872e37
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-22 03:23:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   result = ""
   array.each_with_index do |item, index|
     if array.size > 1
-      result << "item #{index < array.size - 2 ? ", " : " and" }"
+      result << "#{item} #{index < array.size - 2 ? ", " : " and" }"
     end
   end
   result
