decfb901fb13dd13ce671478767d3d73bb005137
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-22 02:55:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   new_array = []
   array.each_with_index do |item, index|
-    puts array.join(index < array.size - 2 ? ", " : " and ")
+    puts array.join(index < array.size - 1 ? ", " : " and ")
   end
 end
