0615bca84c8a686fed17c792d753382ca8a2a3ec
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-22 03:53:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  result = ""
+  array.each_with_index do |item, index|
+    if array.size - 1 == index
+      result << item
+      break
+    end
+    result << item
+    if array.size > 2
+      result << ", "
+    else
+      result << " "
+    end
+    aux = index == array.size - 2 ? "and " : ""
+    result << aux
+  end
+  result
 end
